b415a14a2a4dbcc0523efd577753d76cc0738af2
David Sires <d.sires@yahoo.com>
2017-04-10 12:23:38 -0700

    Assignment-2-Models

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -21,19 +21,36 @@ RSpec.describe AddressBook do
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelave.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
       expect(book.entries.size).to eq(1)
     end
 
     it "adds the correct information to entries" do
       book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelave.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       new_entry = book.entries[0]
 
       expect(new_entry.name).to eq('Ada Lovelace')
       expect(new_entry.phone_number).to eq('010.012.1815')
-      expect(new_entry.email).to eq('augusta.king@lovelave.com')
+      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+
+  describe "#remove_entry" do
+    it "removes only one entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Bob Donahue', '010.102.1815', 'bob.donahue@donahue.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Bob Donahue')
     end
   end
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -2,7 +2,7 @@ require_relative '../models/entry'
 
 RSpec.describe Entry do
   describe "attributes" do
-    let(:entry) {Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelave.com')}
+    let(:entry) {Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')}
 
     it "responds to name" do
       expect(entry).to respond_to(:name)
@@ -25,14 +25,14 @@ RSpec.describe Entry do
     end
 
     it "reports its email" do
-      expect(entry.email).to eq('augusta.king@lovelave.com')
+      expect(entry.email).to eq('augusta.king@lovelace.com')
     end
   end
 
   describe "#to_s" do
     it "prints an entry as a string" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelave.com')
-      expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelave.com"
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
       expect(entry.to_s).to eq(expected_string)
     end
   end
