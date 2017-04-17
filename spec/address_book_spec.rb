73a229217c3093f09164ab4a1e3d638974787d1f
David Sires <d.sires@yahoo.com>
2017-04-16 19:08:47 -0700

    Assignment-4-CSVs

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
-
   let(:book) { AddressBook.new}
 
   def check_entry(entry, expected_name, expected_number, expected_email)
@@ -11,48 +10,6 @@ RSpec.describe AddressBook do
   end
 
   describe "attributes" do
-    it "imports the 1st entry" do
-      book.import_from_csv("entries.csv")
-      entry_one = book.entries[0]
-
-      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-    end
-
-    it "imports the 2nd entry" do
-      book.import_from_csv("entries.csv")
-
-      entry_two = book.entries[1]
-
-      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
-    end
-
-    it "imports the 3rd entry" do
-
-      book.import_from_csv("entries.csv")
-
-      entry_three = book.entries[2]
-
-      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
-    end
-
-    it "imports the 4th entry" do
-
-      book.import_from_csv("entries.csv")
-
-      entry_four = book.entries[3]
-
-      check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
-    end
-
-    it "imports the 5th entry" do
-
-      book.import_from_csv("entries.csv")
-
-      entry_five = book.entries[4]
-
-      check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
-    end
-
     it "responds to entries" do
       expect(book).to respond_to(:entries)
     end
@@ -89,6 +46,63 @@ RSpec.describe AddressBook do
       book_size = book.entries.size
 
       expect(book_size).to eq 5
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+
+    it "imports the 4th entry" do
+      book.import_from_csv("entries.csv")
+      entry_four = book.entries[3]
+      check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+    end
+
+    it "imports the 5th entry" do
+      book.import_from_csv("entries.csv")
+      entry_five = book.entries[4]
+      check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+  end
+
+  describe "import from entries_2.csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
     end
   end
 
