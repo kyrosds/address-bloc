26522b54f9a5915d8b06c150f8967dd5025b0c08
David Sires <d.sires@yahoo.com>
2017-04-18 18:47:53 -0700

    Assigment-5-Searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -2,65 +2,74 @@ require_relative 'entry'
 require "csv"
 
 class AddressBook
-    attr_reader :entries
+  attr_reader :entries
 
-    def initialize
-        @entries = []
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+
+      if name < entry.name
+        break
+      end
+      index += 1
     end
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
 
-    def add_entry(name, phone_number, email)
-        index = 0
-        entries.each do |entry|
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
-            if name < entry.name
-                break
-            end
-            index += 1
-        end
-        entries.insert(index, Entry.new(name, phone_number, email))
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"],  row_hash["phone_number"], row_hash["email"])
     end
+  end
 
-    def import_from_csv(file_name)
-      csv_text = File.read(file_name)
-      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
 
-      csv.each do |row|
-        row_hash = row.to_hash
-        add_entry(row_hash["name"],  row_hash["phone_number"], row_hash["email"])
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
       end
     end
 
-    def remove_entry(name, phone_number, email)
-        delete_entry = nil
+    @entries.delete(delete_entry)
+  end
 
-        @entries.each do |entry|
-            if name == entry.name && phone_number == entry.phone_number && email == entry.email
-                delete_entry = entry
-            end
-        end
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
 
-        @entries.delete(delete_entry)
-    end
+    while lower <= upper
 
-    def binary_search(name)
-      lower = 0
-      upper = entries.length - 1
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
 
-      while lower <= upper
-
-        mid = (lower + upper) / 2
-        mid_name = entries[mid].name
-
-        if name == mid_name
-          return entries[mid]
-        elsif name < mid_name
-          upper = mid - 1
-        elsif name > mid_name
-          lower = mid + 1
-        end
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
       end
-
-      return nil
-
     end
+
+    return nil
+  end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+
+    return nil
+  end
 end
